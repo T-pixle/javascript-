f720d00cee3a56775800343ea47f0fd2772ae063
Tremayne Lee <tremaynelee14@gmail.comgit config --global user.name T-Pixalgit config --list git commit -m initial>
2021-01-30 18:15:18 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,9 @@
+ 
+var firstName = "Tremayne "; 
+var lastName = "Lee ";
+var age = 18;
+
+alert("My name is " + firstName + lastName);
+alert("I am " + age + " years old.");
+
+console. log("I am having so much fun in this class. Breaking the vidoes down make learning how to code much easier and less over whelming. I am ready to continue learning :).");
